ae95decb150ed2f1640213141887ef7d5bd88aca
Alex <hello@alex-ak.com>
2021-03-30 07:43:54 -0700

    Add birthday page and content.

---
+++ client/pages/birthday.tsx
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import styled from "styled-components";
+
+// util
+import { getPageData } from "util/fetchMarkdown";
+
+// types
+import { GetStaticProps } from "next";
+import { PageDataProps } from "types";
+interface Props {
+  pageData: PageDataProps;
+}
+
+const Birthday = ({ pageData }: Props) => {
+  const { title, contentHtml } = pageData;
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+
+      <Styles id="page">
+        <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+      </Styles>
+    </>
+  );
+};
+
+export default Birthday;
+
+const Styles = styled.main``;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const pageData = await getPageData("birthday");
+
+  return { props: { pageData } };
+};
